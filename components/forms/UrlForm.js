fd2732c47fff05a6e4e9f3eab928037cccf4dae5
David White <whited9339@gmail.com>
2022-07-14 13:47:59 -0600

    Create url form component

---
+++ components/forms/UrlForm.js
@@ -0,0 +1,44 @@
+import {
+  TextInput,
+  Card
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+export default ({ urlRec }) => {
+  // const form = useForm({
+  //   initialValues: urlRec
+  //     ? {
+  //       name: urlRec.name,
+  //       redirect_url: urlRec.redirect_url,
+  //       start_date: urlRec.start_date,
+  //       end_date: urlRec.end_date
+  //     } : null,
+  //   validate: ({
+  //     start_date
+  //   }) => ({
+  //     end_date: (date) => date > start_date
+  //   })
+  // });
+
+  return (
+    // <Card>
+    //   <form>
+    //     <TextInput
+    //       label="Name"
+    //       value={form.values.name}
+    //       onChange={(e) => {
+    //         form.setFieldValue("name", e.currentTarget.value);
+    //       }}
+    //     />
+    //     <TextInput
+    //       label="Redirect URL"
+    //       value={form.values.redirect_url}
+    //       onChange={(e) => {
+    //         form.setFieldValue("redirect_url", e.currentTarget.value);
+    //       }}
+    //     />
+    //   </form>
+    // </Card>
+    <div>This is a form. This is where the data is collected.</div>
+  )
+}

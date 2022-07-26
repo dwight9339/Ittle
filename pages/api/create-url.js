81c5e449875f3e42bd14e9aec37410ec9fa58a25
David White <whited9339@gmail.com>
2022-07-26 15:52:15 -0600

    Create-url route retries request in case of key collision

--- pages/api/create-url.js
+++ pages/api/create-url.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoServerError } from "mongodb";
 import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 import generateSlug from "utils/generateSlug";
 
@@ -6,6 +6,7 @@ const createUrl = withApiAuthRequired(async (req, res) => {
   if (req.method !== "POST") {
     return res.status(401).send();
   }
+
   const { user } = getSession(req, res);
 
   const client = new MongoClient(process.env.MONGODB_URI);
@@ -15,17 +16,23 @@ const createUrl = withApiAuthRequired(async (req, res) => {
   redirectData.user_id = user.sub;
   redirectData.click_count = 0;
   
-  console.log(JSON.stringify(redirectData));
   try {
     const db = client.db(process.env.MONGODB_DB);
     const redirects = db.collection("redirects");
-
-    const result = await redirects.insertOne(redirectData);
+    await redirects.insertOne(redirectData);
     
     res.status(200).send();
   } catch(err) {
-    console.error(err);
-    res.status(500).send("Unable to insert record");
+    if (err instanceof MongoServerError && err.code === 11000) { 
+      // Key collision error, reissue request
+      console.error("Key collision, retrying request");
+      res.redirect(307, "/api/create-url");
+    } else {
+      console.error(err.code);
+      res.status(500).send("Unable to complete transaction");
+    }
+  } finally {
+    await client.close();
   }
 });
 
